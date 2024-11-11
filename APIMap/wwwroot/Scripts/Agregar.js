1fb93c05a1460f39b2857141203a75642f4e0270
AlexanderContreras17 <hatakealexander24@gmail.com>
2024-11-11 12:07:17 -0600

    cambios vistas

--- APIMap/wwwroot/Scripts/Agregar.js
+++ APIMap/wwwroot/Scripts/Agregar.js
@@ -1,1 +1,28 @@
-﻿
+﻿//let from = document.querySelector("from");
+let btn = document.getElementById("#nombre");
+let lblerror = document.querySelector(".error");
+btn.addEventListener("click", async function (e) {
+    let descr = document.getElementById("#descripcion")
+    if (descr.checkValidity()) {
+        lblerror.textContent = "";
+        let json = { descripcion = descripcion.value, nombre=nombre.value };
+        let result = await fetch("api/Area", {
+            method: "post",
+            body: JSON.stringify(json),
+            headers: {
+                "Content-Type": "aplication/json"
+            }
+        });
+        if (result.ok) {
+            window.location.replace("Admin/Index");
+
+        }
+        else if (result.status == 400) {
+            lblerror.textContent = "Verifique el mensaje";
+        }
+    }
+    else {
+        descr.reportValidity();
+
+    }
+});
