4db6a8e59693a865637e2176d28a068a4c04ee98
AlexanderContreras17 <hatakealexander24@gmail.com>
2024-11-12 03:13:05 -0600

    modificacion del index

--- APIMap/Program.cs
+++ APIMap/Program.cs
@@ -11,18 +11,18 @@ x.UseMySql(connection, ServerVersion.AutoDetect(connection)));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+
 builder.Services.AddControllers();
 builder.Services.AddRazorPages();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-	app.UseSwagger();
-	app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//	app.UseSwagger();
+//	app.UseSwaggerUI();
+//}
 
 app.UseDeveloperExceptionPage();
 
