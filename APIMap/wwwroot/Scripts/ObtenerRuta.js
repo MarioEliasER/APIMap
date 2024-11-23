cebce0043b3cff2f620ec6f0297491d4146174d5
AlexanderContreras17 <hatakealexander24@gmail.com>
2024-11-23 13:16:11 -0600

    Generar Rutas, funcionando, falta cargar bien los nombres y las img de las rutas

---
+++ APIMap/wwwroot/Scripts/ObtenerRuta.js
@@ -0,0 +1,14 @@
+﻿async function obtenerRuta() {
+    let rutaSelector = document.querySelector('#Ruta');
+    let origen = rutaSelector[rutaSelector.selectedIndex].textContent;
+    let destinoSelector = document.querySelector('#Destino');
+    let destino = destinoSelector[destinoSelector.selectedIndex].textContent;
+    ObtenerImagen(origen, destino);
+}
+let url_local = "https://localhost:44380/";
+async function ObtenerImagen(origen, destino) {
+    //https://localhost:44380/Rutas/Edificio de sistemas -Biblioteca
+    let imagen = document.querySelector(".ImgRuta");
+    imagen.src = url_local + 'Rutas/' + origen + '-' + destino + '.png';
+}
+
